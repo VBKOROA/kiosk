810539d5597e40d7b05585f37d7e8749399d1a54
abko <abko121115@gmail.com>
2025-07-16 12:02:19 +0900

    feat(Kiosk): 클래스 생성

---
+++ app/src/main/java/kiosk/service/Kiosk.java
@@ -0,0 +1,14 @@
+package kiosk.service;
+
+import java.util.List;
+
+import kiosk.models.MenuItem;
+
+public class Kiosk {
+    private List<MenuItem> menuItems = List.of(
+                new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
+                new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
+                new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"),
+                new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거")
+        );
+}
