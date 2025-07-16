b52c7d3e5e15c9c56ffbea2aa8edeb42a489b909
abko <abko121115@gmail.com>
2025-07-16 16:11:34 +0900

    docs(MenuManager): JAVADOCS 작성

--- app/src/main/java/kiosk/managers/MenuManager.java
+++ app/src/main/java/kiosk/managers/MenuManager.java
@@ -17,11 +17,20 @@ public class MenuManager {
         this.menuItems.addAll(menuItems);
     }
 
+    /**
+     * 메뉴 아이템들을 반환한다.
+     * @return 수정 불가능한 메뉴 아이템 리스트
+     */
     public List<MenuItem> getMenuItems() {
         // 수정 불가능한 리스트를 반환
         return List.copyOf(menuItems);
     }
 
+    /**
+     * 카테고리에 해당하는 메뉴 아이템들을 반환한다.
+     * @param category
+     * @return 수정 불가능한 메뉴 아이템 리스트
+     */
     public List<MenuItem> getMenuItemsByCategory(MenuCategory category) {
         return menuItems.stream()
                 .filter(item -> item.category().equals(category)) // 카테고리로 필터링
