ba8e4a8256ef6c67c82f791176ba11c04dbe3637
abko <abko121115@gmail.com>
2025-07-16 12:46:17 +0900

    feat(Kiosk): menuSelectMenu 구현

--- app/src/main/java/kiosk/service/Kiosk.java
+++ app/src/main/java/kiosk/service/Kiosk.java
@@ -16,45 +16,11 @@ public class Kiosk {
     }
 
     public void start() {
-        while (true) {
-            System.out.println("[ SHAKESHACK MENU ]");
 
-            for (int i = 0; i < menuManager.getMenuSize(); i++) {
-                System.out.println(i + 1 + ". " + menuManager.getMenuItemFromIdx(i));
-            }
-
-            System.out.println("0. 종료 | 종료");
-
-            int choice;
-
-            try {
-                choice = sc.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
-                // 잘못 입력된 버퍼를 청소
-                sc.next();
-                continue;
-            }
-
-            if (choice == 0) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            }
-
-            try {
-                menuManager.getMenuItemFromIdx(choice - 1);
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-
-            MenuItem selectedItem = menuManager.getMenuItemFromIdx(choice - 1);
-            selectedItem.selected();
-        }
     }
 
     private void mainMenu() {
-        while(true) {
+        while (true) {
             System.out.println("[ MAIN MENU ]");
             var categories = menuManager.getMenuCategories();
 
@@ -88,5 +54,39 @@ public class Kiosk {
     }
 
     private void menuSelectMenu(String category) {
+        var items = menuManager.getMenuItemsByCategory(category);
+        while (true) {
+            System.out.println("[ SHAKESHACK MENU ]");
+
+            for (int i = 0; i < items.size(); i++) {
+                System.out.println(i + 1 + ". " + items.get(i));
+            }
+
+            System.out.println("0. 종료 | 종료");
+
+            int choice;
+
+            try {
+                choice = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+                // 잘못 입력된 버퍼를 청소
+                sc.next();
+                continue;
+            }
+
+            if (choice == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            if (choice < 1 || choice > items.size()) {
+                System.out.println("잘못된 선택입니다. 다시 시도해주세요.");
+                continue;
+            }
+
+            MenuItem selectedItem = items.get(choice - 1);
+            selectedItem.selected();
+        }
     }
 }
