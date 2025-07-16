418cc29f2be509d165b4af3d59a74666719fd4a1
abko <abko121115@gmail.com>
2025-07-16 13:42:12 +0900

    continue

--- app/src/main/java/kiosk/managers/CartManager.java
+++ app/src/main/java/kiosk/managers/CartManager.java
@@ -1,5 +1,10 @@
 package kiosk.managers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import kiosk.models.MenuItem;
+
 public class CartManager {
-    
+    private final Map<MenuItem, Integer> cartItems = new HashMap<>();
 }
