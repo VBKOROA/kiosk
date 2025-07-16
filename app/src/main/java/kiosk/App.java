681a2a9a988e3073230915fbf30f16e084c65e64
abko <abko121115@gmail.com>
2025-07-16 11:12:01 +0900

    feat: lvl1 기본적인 키오스크 작성

--- app/src/main/java/kiosk/App.java
+++ app/src/main/java/kiosk/App.java
@@ -3,12 +3,51 @@
  */
 package kiosk;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("""
+                    [ SHAKESHACK MENU ]
+                    1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거
+                    2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거
+                    3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거
+                    4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거
+                    0. 종료          | 종료  | 종료합니다.
+                    """);
+
+            int choice;
+            try {
+                choice = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+                // 잘못 입력된 버퍼를 청소
+                sc.next();
+                continue;
+            }
+            
+            switch (choice) {
+                case 1:
+                    System.out.println("ShackBurger를 선택하셨습니다.");
+                    break;
+                case 2:
+                    System.out.println("SmokeShack를 선택하셨습니다.");
+                    break;
+                case 3:
+                    System.out.println("Cheeseburger를 선택하셨습니다.");
+                    break;
+                case 4:
+                    System.out.println("Hamburger를 선택하셨습니다.");
+                    break;
+                case 0:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못된 선택입니다.");
+            }
+        }
     }
 }
