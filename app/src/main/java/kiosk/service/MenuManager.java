ed5fded233373e3ef905238853ef07b5d6d773d5
abko <abko121115@gmail.com>
2025-07-16 12:20:45 +0900

    feat(MenuManager): getter와 setter(adder)구현

--- app/src/main/java/kiosk/service/MenuManager.java
+++ app/src/main/java/kiosk/service/MenuManager.java
@@ -1,6 +1,7 @@
 package kiosk.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import kiosk.models.MenuItem;
@@ -10,5 +11,14 @@ public class MenuManager {
 
     public MenuManager(List<MenuItem> menuItems) {
         this.menuItems.addAll(menuItems);
+    }
+
+    public List<MenuItem> getMenuItems() {
+        // 수정 불가능한 리스트를 반환
+        return Collections.unmodifiableList(menuItems);
+    }
+
+    public void addMenuItem(MenuItem item) {
+        menuItems.add(item);
     }
 }
