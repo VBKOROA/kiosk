98dcc51894f72be90f4ad05bc51d6a09d29c963c
abko <abko121115@gmail.com>
2025-07-16 12:30:42 +0900

    feat(App): 음료, 사이드 카테고리의 메뉴 추가

--- app/src/main/java/kiosk/App.java
+++ app/src/main/java/kiosk/App.java
@@ -14,10 +14,21 @@ public class App {
         Scanner sc = new Scanner(System.in);
         
         List<MenuItem> menuItems = List.of(
-                new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거", "버거"),
-                new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거", "버거"),
-                new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", "버거"),
-                new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거", "버거")
+            // 버거 메뉴
+            new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거", "버거"),
+            new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거", "버거"),
+            new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", "버거"),
+            new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거", "버거"),
+
+            // 음료 메뉴
+            new MenuItem("Shack Shake", 4.5, "바닐라 아이스크림과 우유로 만든 쉐이크", "음료"),
+            new MenuItem("Chocolate Shake", 4.5, "초콜릿 아이스크림과 우유로 만든 쉐이크", "음료"),
+            new MenuItem("Strawberry Shake", 4.5, "딸기 아이스크림과 우유로 만든 쉐이크", "음료"),
+
+            // 사이드 메뉴
+            new MenuItem("French Fries", 3.5, "바삭한 감자튀김", "사이드"),
+            new MenuItem("Cheese Fries", 4.0, "치즈가 듬뿍 얹어진 감자튀김", "사이드"),
+            new MenuItem("Onion Rings", 3.5, "바삭한 양파링", "사이드")
         );
 
         Kiosk kiosk = new Kiosk(menuItems, sc);
