aa69586a35ee1e0dac4a9cc0498456f8a96209a5
abko <abko121115@gmail.com>
2025-07-16 12:19:29 +0900

    feat(MenuManager): 생성자 구현

--- app/src/main/java/kiosk/service/MenuManager.java
+++ app/src/main/java/kiosk/service/MenuManager.java
@@ -7,4 +7,8 @@ import kiosk.models.MenuItem;
 
 public class MenuManager {
     private final List<MenuItem> menuItems = new ArrayList<>();
+
+    public MenuManager(List<MenuItem> menuItems) {
+        this.menuItems.addAll(menuItems);
+    }
 }
