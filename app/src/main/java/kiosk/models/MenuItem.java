01d8848e70794493bc0b67ebbf0e58fb57aec1d6
abko <abko121115@gmail.com>
2025-07-16 11:30:36 +0900

    fix(AppTest): 불필요한 test코드 삭제 feat(MenuItem): toString메서드 오버라이딩 refactor(App): MenuItme기반 객체 활용 수정

--- app/src/main/java/kiosk/models/MenuItem.java
+++ app/src/main/java/kiosk/models/MenuItem.java
@@ -4,4 +4,15 @@ public record MenuItem(
         String name, // 이름
         double price, // 가격
         String description // 설명
-) {}
+) {
+        @Override
+        public String toString() {
+                StringBuilder sb = new StringBuilder();
+                sb.append(name)
+                    .append(" | W ")
+                    .append(price)
+                    .append(" | ")
+                    .append(description);
+                return sb.toString();
+        }
+}
