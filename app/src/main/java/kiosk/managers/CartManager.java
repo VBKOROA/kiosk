d4938fdbb72081c57d5901201d19f6d08a1f2243
abko <abko121115@gmail.com>
2025-07-16 13:45:08 +0900

    feat(CartManager): addItem구현

--- app/src/main/java/kiosk/managers/CartManager.java
+++ app/src/main/java/kiosk/managers/CartManager.java
@@ -7,4 +7,9 @@ import kiosk.models.MenuItem;
 
 public class CartManager {
     private final Map<MenuItem, Integer> cartItems = new HashMap<>();
+
+    public void addItem(MenuItem item, int quantity){
+        cartItems.merge(item, quantity, Integer::sum); // 메서드 참조를 사용
+            // (oldValue, newValue) -> oldValue + newValue랑 같은 의미
+    }
 }
