d3f3675da55718066e9526f333b2c2299941cc4b
abko <abko121115@gmail.com>
2025-07-16 12:19:08 +0900

    feat(MenuManager): 클래스 생성

---
+++ app/src/main/java/kiosk/service/MenuManager.java
@@ -0,0 +1,10 @@
+package kiosk.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kiosk.models.MenuItem;
+
+public class MenuManager {
+    private final List<MenuItem> menuItems = new ArrayList<>();
+}
