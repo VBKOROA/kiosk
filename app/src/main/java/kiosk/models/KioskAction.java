fc64528fdf73591c7f6736a905c92b9e424cd9b4
abko <abko121115@gmail.com>
2025-07-17 12:34:52 +0900

    feat(KioskAction): KioskAction 인터페이스 구현

---
+++ app/src/main/java/kiosk/models/KioskAction.java
@@ -0,0 +1,16 @@
+package kiosk.models;
+
+import kiosk.enums.MenuCategory;
+import kiosk.enums.SaleCategory;
+
+public sealed interface KioskAction {
+    record MainMenu() implements KioskAction {}
+    record ProgramExit() implements KioskAction {}
+    record CancelItems() implements KioskAction {}
+    record MenuSelectMenu(MenuCategory category) implements KioskAction {}
+    record AddItemToCartMenu(MenuItem item) implements KioskAction {}
+    record AddToCart(MenuItem item) implements KioskAction {}
+    record CartCheckBeforeOrder() implements KioskAction {}
+    record DiscountMenu() implements KioskAction {}
+    record ProcessingOrder(SaleCategory category) implements KioskAction {}
+}
