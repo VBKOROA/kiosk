17829965e5ce774d8136af92e76f0f57c2353e47
abko <abko121115@gmail.com>
2025-07-17 09:59:41 +0900

    fix(KioskUI): 불필요한 Entry 임포트 제거

--- app/src/main/java/kiosk/ui/KioskUI.java
+++ app/src/main/java/kiosk/ui/KioskUI.java
@@ -3,7 +3,6 @@ package kiosk.ui;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
 import kiosk.enums.MenuCategory;
