614d1076c73af94881daa4fd38dacc7c06360297
abko <abko121115@gmail.com>
2025-07-18 22:13:28 +0900

    docs(ActionHandler): 핸들러 메서드에 대한 Javadoc 주석 추가

--- app/src/main/java/kiosk/handler/ActionHandler.java
+++ app/src/main/java/kiosk/handler/ActionHandler.java
@@ -3,5 +3,10 @@ package kiosk.handler;
 import kiosk.model.KioskAction;
 
 public interface ActionHandler {
+    /**
+     * 핸들러의 특정 로직을 실행하고, 다음으로 수행할 KioskAction을 반환.
+     *
+     * @return 다음으로 실행될 KioskAction 객체
+     */
     public KioskAction handle();
 }
