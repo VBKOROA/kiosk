c1ef5135c0fe84a02c0c56133ed274ce25f0d833
abko <abko121115@gmail.com>
2025-07-18 11:51:31 +0900

    refactor(ItemAddedToCartUI): 파라미터 DTO 제거 및 팩토리 메서드 시그니처 수정

--- app/src/main/java/kiosk/ui/display/ItemAddedToCartUI.java
+++ app/src/main/java/kiosk/ui/display/ItemAddedToCartUI.java
@@ -13,11 +13,11 @@ public class ItemAddedToCartUI implements Displayable {
     /**
      * ItemAddedToCartUI의 인스턴스를 생성하는 팩토리 메서드.
      * 
-     * @param parameter
+     * @param item 장바구니에 추가된 메뉴 아이템
      * @return ItemAddedToCartUI 인스턴스
      */
-    public static ItemAddedToCartUI withParameter(ParameterDto parameter) {
-        return new ItemAddedToCartUI(parameter.item());
+    public static ItemAddedToCartUI withParameter(MenuItem item) {
+        return new ItemAddedToCartUI(item);
     }
 
     /**
@@ -27,13 +27,5 @@ public class ItemAddedToCartUI implements Displayable {
     public void display() {
         System.out.println();
         System.out.println("장바구니에 " + item.name() + "이 추가되었습니다.");
-    }
-
-    /**
-     * ItemAddedToCartUI의 파라미터 DTO 클래스.
-     * 
-     * @param item 장바구니에 추가된 메뉴 아이템
-     */
-    public static record ParameterDto(MenuItem item) {
     }
 }
