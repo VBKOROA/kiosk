59f24a653dc1ef0313045e6836777bf514577834
abko <abko121115@gmail.com>
2025-07-18 22:13:36 +0900

    docs(Kiosk): run 메서드에 대한 Javadoc 주석 추가

--- app/src/main/java/kiosk/service/Kiosk.java
+++ app/src/main/java/kiosk/service/Kiosk.java
@@ -10,6 +10,11 @@ public class Kiosk {
         this.handlerFactory = handlerFactory;
     }
 
+    /**
+     * 키오스크의 메인 실행 루프를 시작.
+     * 현재 상태(KioskAction)를 핸들러 팩토리에 전달하여 다음 핸들러를 가져오고,
+     * 이를 실행하는 과정을 프로그램이 종료될 때까지 반복.
+     */
     public void run() {
         KioskAction curAction = new KioskAction.MainMenu();
         while (true) {
