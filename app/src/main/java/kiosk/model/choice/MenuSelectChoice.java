4ffe17303631b4be5a6c0173ecf99f1c5244d405
abko <abko121115@gmail.com>
2025-07-23 17:07:59 +0900

    feat: MenuSelectChoice에 process 메서드 추가 및 GoBack/SelectThis 동작 구현

--- app/src/main/java/kiosk/model/choice/MenuSelectChoice.java
+++ app/src/main/java/kiosk/model/choice/MenuSelectChoice.java
@@ -1,18 +1,36 @@
 package kiosk.model.choice;
 
+import kiosk.handler.MenuSelectMenuHandler;
 import kiosk.model.MenuItem;
+import kiosk.model.action.AddItemToCartMenuAction;
+import kiosk.model.action.KioskAction;
+import kiosk.model.action.MainMenuAction;
 
 /**
  * 메뉴 선택 화면에서 선택할 수 있는 선택지를 나타냄
  */
 public sealed interface MenuSelectChoice extends Choice {
+    KioskAction process(MenuSelectMenuHandler handler);
+
     /**
      * 이전 화면으로 돌아가는 선택지
      */
-    record GoBack() implements MenuSelectChoice {}
+    record GoBack() implements MenuSelectChoice {
+        @Override
+        public KioskAction process(MenuSelectMenuHandler handler) {
+            return new MainMenuAction();
+        }
+    }
+
     /**
      * 특정 메뉴 항목을 선택하는 선택지
+     * 
      * @param item 선택된 메뉴 항목
      */
-    record SelectThis(MenuItem item) implements MenuSelectChoice {}
+    record SelectThis(MenuItem item) implements MenuSelectChoice {
+        @Override
+        public KioskAction process(MenuSelectMenuHandler handler) {
+            return new AddItemToCartMenuAction(item);
+        }
+    }
 }
